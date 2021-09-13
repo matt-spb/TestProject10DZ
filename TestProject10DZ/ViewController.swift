4543c5369d9fdc57c7ab15543a6aa831159a79b7
Matvey <mnf89@mail.ru>
2021-09-13 23:56:48 +0300

    create setupView method

--- TestProject10DZ/ViewController.swift
+++ TestProject10DZ/ViewController.swift
@@ -11,6 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupView()
+    }
+
+    private func setupView() {
+        view.backgroundColor = .systemRed
     }
 
 }
