bee34deb0b913ec40c21b84a0bfbff490395eff4
Matvey <mnf89@mail.ru>
2021-09-13 23:09:38 +0300

    Initial Commit

---
+++ TestProject10DZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject10DZ
+//
+//  Created by matt_spb on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
