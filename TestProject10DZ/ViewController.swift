8d36dc4d964ebe6d5b05fc1185c5b832dc825828
Matvey <mnf89@mail.ru>
2021-09-14 00:04:53 +0300

    delete all commented strings

--- TestProject10DZ/ViewController.swift
+++ TestProject10DZ/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  TestProject10DZ
-//
-//  Created by matt_spb on 13.09.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
